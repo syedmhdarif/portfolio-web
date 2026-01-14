66f4ac0aab4ccade7b439d9bd9d85b3f7aa8c03a
syedmhdarif <syedarifjr@gmail.com>
2026-01-14 10:01:39 +0800

    Configure SSR deployment to Cloudflare Workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    cloudflareDevProxy(),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });
