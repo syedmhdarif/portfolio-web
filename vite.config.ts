543114d6a6e64b1f50732646d660135a326bf2cf
syedmhdarif <syedarifjr@gmail.com>
2026-01-18 20:56:14 +0800

    add:github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: process.env.BASE_PATH || "/",
   plugins: [
     cloudflareDevProxy(),
     tailwindcss(),
